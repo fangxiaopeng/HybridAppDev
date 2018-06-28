312cd7a812bbe0607953cbcdbe08cfdb51c7d6d1
fangxiaopeng <850899969@qq.com>
2018-06-28 09:34:15 +0800

    提交MyFileProvider.java

---
+++ HybridAppDev/cordova/src/main/java/com/example/cordova/provider/MyFileProvider.java
@@ -0,0 +1,27 @@
+package com.example.cordova.provider;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Title:       MyFileProvider
+ * <p>
+ * Package:     com.example.cordova.provider
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/6/27 上午4:12
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/6/27    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class MyFileProvider extends FileProvider {
+
+}
