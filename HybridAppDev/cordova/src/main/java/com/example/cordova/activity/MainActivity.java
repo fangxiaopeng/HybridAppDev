3df186f2a5f9ec50f5ae891f66cc3d76a1e31754
fangxiaopeng <850899969@qq.com>
2018-06-28 15:04:24 +0800

    activity-->fragment中实现cordova webview

--- HybridAppDev/cordova/src/main/java/com/example/cordova/activity/MainActivity.java
+++ HybridAppDev/cordova/src/main/java/com/example/cordova/activity/MainActivity.java
@@ -4,12 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.cordova.R;
+import com.example.cordova.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.root_layout, new MainFragment()).commit();
     }
+
 }
