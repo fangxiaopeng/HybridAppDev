98b158848ea5050b100413026fc1635a0bcc0da2
fangxiaopeng <850899969@qq.com>
2018-07-26 16:41:45 +0800

    update ModuleExpand.js

--- HybridAppDev/weex/src/main/assets/android-expand-demo/ModuleExpand.js
+++ HybridAppDev/weex/src/main/assets/android-expand-demo/ModuleExpand.js
@@ -6,11 +6,30 @@
 
 <script>
   module.exports = {
+    data: {},
     methods: {
       click: function() {
         weex.requireModule('MyModule').printLog("I am a weex Module");
       }
+    },
+    init: function () {
+      console.log('在初始化内部变量，并且添加了事件功能后被触发');
+    },
+    created: function () {
+      console.log('完成数据绑定之后，模板编译之前被触发');
+    },
+    ready: function () {
+      console.log('模板已经编译并且生成了 Virtual DOM 之后被触发');
+    },
+    destroyed: function () {
+      console.log('在页面被销毁时调用');
     }
+  }
+</script>
+
+<script>
+  module.exports = {
+
   }
 </script>
 
