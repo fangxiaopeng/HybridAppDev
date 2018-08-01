67ee48b943e8a76f36f7cd547c80c293a6f8e792
fangxiaopeng <850899969@qq.com>
2018-08-01 16:46:03 +0800

    Issue Weex页面加载失败，onException error Code: -1001

--- HybridAppDev/weex_sdk/src/main/java/com/taobao/weex/bridge/WXBridgeManager.java
+++ HybridAppDev/weex_sdk/src/main/java/com/taobao/weex/bridge/WXBridgeManager.java
@@ -28,6 +28,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.util.ArrayMap;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -1536,7 +1537,12 @@ public class WXBridgeManager implements Callback, BactchExecutor {
       return;
     }
 
+    Log.e("fxp", "isJSFrameworkInit()-" + isJSFrameworkInit());  // false
+    Log.e("fxp", "reInitCount-" + reInitCount);                 // 1
+    Log.e("fxp", "WXEnvironment.sDebugServerConnectable-" + WXEnvironment.sDebugServerConnectable);   // false
+
     if (!isJSFrameworkInit() && reInitCount == 1 && !WXEnvironment.sDebugServerConnectable) {
+      Log.e("fxp", "instance.onRenderError");
       instance.onRenderError(
 			  WXRenderErrorCode.DegradPassivityCode.WX_DEGRAD_ERR_INSTANCE_CREATE_FAILED.getDegradErrorCode(),
 			  WXRenderErrorCode.DegradPassivityCode.WX_DEGRAD_ERR_INSTANCE_CREATE_FAILED.getDegradErrorMsg() +
@@ -1547,7 +1553,7 @@ public class WXBridgeManager implements Callback, BactchExecutor {
           initFramework("");
         }
       }, instanceId);
-      return;
+      return;         // 注销此处return即可解决页面空白问题
     }
 
     WXModuleManager.createDomModule(instance);
